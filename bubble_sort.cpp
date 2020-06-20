1e29ce4c61fd51ed9b8e368aac02d323799c2fb7
vaijayanthi-01 <66869524+vaijayanthi-01@users.noreply.github.com>
2020-06-20 17:37:11 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -29,15 +29,20 @@ void swap(int *x, int *y)
 
 void bubbleSort(int arr[], int size)
 {
-  int i, j;
+  int i, j, flag = 0;
   
   for(i = 0; i < size - 1; ++i)
   {
     for(j = 0; j < size - i - 1; ++j)
     {
       if(arr[j] > arr[j + 1])
+      {
         swap(arr[j], arr[j + 1]);
+        flag = 1;
+      }
     }
+    if(flag == 0)
+      break;
   }
   
 }
